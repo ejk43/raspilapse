a4da4e3fca60376f64511beca4bdd09afe9eaec6
EJ Kreinar <ejkreinar@gmail.com>
2018-01-28 20:40:43 +0000

    updated run_timelapse.sh

--- run_timelapse.sh
+++ run_timelapse.sh
@@ -5,5 +5,5 @@
 
 set -x
 sleep 60s # Wait before starting so the raspberry pi can query internet time
-/home/pi/raspilapse/timelapse.py -s -o /home/pi/raspilapse/pictures -t 10 -d 0.5 && \
+/home/pi/raspilapse/timelapse.py -s -o /home/pi/raspilapse/pictures -t 10 -d 480
 /home/pi/raspilapse/convert_to_avi.sh $(ls -td /home/pi/raspilapse/pictures/*/ | head -n 1) 10
